b8183552e4b5e504586854f95665b97f59fbcbbb
dru1 <dru1441@gmail.com>
2021-04-10 10:35:13 +0200

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     java
     id("maven-publish")
     war
-    id("org.springframework.boot") version "2.2.4.RELEASE"
+    id("org.springframework.boot") version "2.4.4"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 repositories {
@@ -13,33 +14,30 @@ repositories {
 
 dependencies {
     // Wicket
-    implementation("org.apache.wicket:wicket-core:8.7.0")
-    implementation("org.apache.wicket:wicket-auth-roles:8.7.0")
-    implementation("org.apache.wicket:wicket-spring:8.7.0")
-    implementation("org.apache.wicket:wicket-extensions:8.7.0")
-    implementation("org.apache.wicket:wicket-devutils:8.7.0")
+    implementation("org.apache.wicket:wicket-core:8.12.0")
+    implementation("org.apache.wicket:wicket-auth-roles:8.12.0")
+    implementation("org.apache.wicket:wicket-spring:8.12.0")
+    implementation("org.apache.wicket:wicket-extensions:8.12.0")
+    implementation("org.apache.wicket:wicket-devutils:8.12.0")
 
     // Spring
-    implementation("org.springframework.boot:spring-boot-starter-web:2.2.4.RELEASE")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.2.4.RELEASE")
-
-    // Databases
-    implementation("org.hsqldb:hsqldb:2.5.0")
-    implementation("mysql:mysql-connector-java:8.0.19")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     // Utils
     implementation("com.google.code.findbugs:jsr305:3.0.2")
-    implementation("com.google.guava:guava:28.2-jre")
-    implementation("org.apache.commons:commons-lang3:3.9")
+    implementation("com.google.guava:guava:30.1.1-jre")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+
+    // Databases
+    runtimeOnly("org.hsqldb:hsqldb:2.5.1")
+    runtimeOnly("mysql:mysql-connector-java:8.0.23")
 
     // Tests
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.2.4.RELEASE")
-
-    // Spring Boot
-    providedCompile("org.springframework.boot:spring-boot-starter-tomcat:2.2.4.RELEASE")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     // JPA
-    annotationProcessor("org.hibernate:hibernate-jpamodelgen:5.4.10.Final")
+    annotationProcessor("org.hibernate:hibernate-jpamodelgen")
 }
 
 group = "at.dru"
@@ -61,6 +59,10 @@ publishing {
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks.bootRun {
